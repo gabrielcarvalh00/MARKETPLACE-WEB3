0ec806de34ab0fe0ccee1ae971a4546ea3d70daa
gabrielcarvalh00 <gcsantos.cic@uesc.br>
2025-05-06 23:51:47 -0300

    new commit

--- server.js
+++ server.js
@@ -22,29 +22,23 @@ const con=mysql.createConnection({
 const app = express();
 
 //especificing where foldr html
-app.use(express.static('./public')); 
+app.use(express.static('./public'));
 
+app.post('/insert', (req, res) => {
+  const { AddressServer, Value, Description, NameOwer, WalletAddress, DroneBrand, LocalLat, LocalLong, DateRegister } = req.body;
 
-app.get('/cidades', (req, res) => {
-  const cidade = req.query.cidade;
-  con.query('SELECT city, address FROM image WHERE city LIKE ?', [`%${cidade}%`], (err, results) => {
+  const query = `INSERT INTO Image (address, description, drone_manufacturer, location_lat, location_lon, seller_id, registrationdata) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+
+  con.query(query, [AddressServer, Description, DroneBrand, LocalLat, LocalLong, NameOwer, DateRegister], (err, result) => {
     if (err) {
-      return res.status(500).send('Erro ao acessar o banco de dados');
+      return res.status(500).send('Erro ao inserir dados'); // Resposta de erro
     }
-    res.json(results);
+    res.status(200).send('Dados inseridos com sucesso'); // Resposta de sucesso
   });
 });
 
-/*
-app.get('/cidades', (req, res) => {
-  con.query('SELECT *FROM image', (err, results) => {
-    if (err) {
-      return res.status(500).send('Erro ao acessar o banco de dados');
-    }
-    res.json(results); // Ex: [{ city: "Campinas" }, ...]
-  });
-});
-*/
+
+
 
 app.listen(3030, () => {
   console.log('Running server');
